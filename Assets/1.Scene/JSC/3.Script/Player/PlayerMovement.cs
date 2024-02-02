5e9fd3da4b90af586d8045037b2505f93b67c7a8
SungChanJoo <scjoo123@naver.com>
2024-02-02 16:24:04 +0900

    [fix] 플레이어 이동 Rigidbody로 교체
    -기존 position직접 변경에서 rigidbody.AddForce()로 변경
    -카메라 시네머신 2D camera으로 플레이어 추적

--- Assets/1.Scene/JSC/3.Script/Player/PlayerMovement.cs
+++ Assets/1.Scene/JSC/3.Script/Player/PlayerMovement.cs
@@ -12,19 +12,34 @@ public enum ButtonState
 
 public class PlayerMovement : MonoBehaviour
 {
+    public float acceleration = 10f;
+    public float decceleration = 1f;
+    public float velPower = 1f;
+
+    float moveInput;
+
+    public float MoveSpeed = 0f;
     public bool IsPress = false;
     ButtonState buttonState = ButtonState.None;
-    public float MoveSpeed = 0f;
+
+    Rigidbody _rb;
+    private void Awake()
+    {
+        _rb = GetComponent<Rigidbody>();
+    }
     //버튼을 눌렀을 때
     public void OnRightButtonDown()
     {
         IsPress = true;
+        moveInput = Vector3.right.x;
         if (buttonState != ButtonState.Right)
             buttonState = ButtonState.Right;
     }
     public void OnLeftButtonDown()
     {
         IsPress = true;
+        moveInput = Vector3.left.x;
+
         if (buttonState != ButtonState.Left)
             buttonState = ButtonState.Left;
     }
@@ -36,18 +51,30 @@ public class PlayerMovement : MonoBehaviour
 
     private void Update()
     {
+        //이동하고자 하는 방향과 목표 속도를 계산
+        float targetSpeed = moveInput * MoveSpeed;
+        //현재 속도와 목표 속도 사이의 차이를 계산
+        float speedDif = targetSpeed - _rb.velocity.x;
+        //상황에 따라 가속도를 변경
+        float accelRate = (Mathf.Abs(targetSpeed) > 0.01f) ? acceleration : decceleration;
+        //가속도를 속도 차이에 적용하고, 특정한 지수로 제곱하여 가속도를 조절
+        float movement = Mathf.Pow(Mathf.Abs(speedDif) * accelRate, velPower) * Mathf.Sign(speedDif);
+
         if (IsPress && buttonState == ButtonState.Right)
         {
-            transform.position += Vector3.right * Time.deltaTime * MoveSpeed;
+            //Rigidbody에 힘을 적용하며, Vector2.right를 곱하여 X 축에만 영향을 줍
+            _rb.AddForce(movement * Vector3.right);
             transform.rotation = Quaternion.Euler(0f, -90f, 0f);
         }
         if (IsPress && buttonState == ButtonState.Left)
         {
-            transform.position += Vector3.left * Time.deltaTime * MoveSpeed;
+            //Rigidbody에 힘을 적용하며, Vector2.right를 곱하여 X 축에만 영향을 줍
+            _rb.AddForce(movement * Vector3.right);
             transform.rotation = Quaternion.Euler(0f, 90f, 0f);
-
+        }
+        if(!IsPress)
+        {
+            _rb.velocity = Vector3.zero;
         }
     }
-
-    
 }
