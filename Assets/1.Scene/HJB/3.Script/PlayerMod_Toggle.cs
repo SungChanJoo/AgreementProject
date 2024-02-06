1e3e8fdc8fadad3f2d0323693c0688ed965bcde0
JaeBack-HONG <khks33@naver.com>
2024-02-06 16:30:11 +0900

    [Feat]MetaWorld_UI 및 Shop_UI 제작

--- Assets/1.Scene/HJB/3.Script/PlayerMod_Toggle.cs
+++ Assets/1.Scene/HJB/3.Script/PlayerMod_Toggle.cs
@@ -6,14 +6,49 @@ public class PlayerMod_Toggle : MonoBehaviour
 {
     [SerializeField] private float setTime = 0f;
     [SerializeField] private GameObject select_obj;
-    [SerializeField] private int playerMod = 1;
-    RectTransform rectTransform;    
+    [SerializeField] private GameObject[] next_obj;
+    [SerializeField] private GameObject filter_Canvas;
+    [SerializeField] private GameObject nonfilter_Canvas;
+    private int playerMod = 1;    
+    RectTransform rectTransform;
+    RectTransform[] rects;
+    Vector3 nextVector3;
     float nextVector = 0;
+    bool onFilter = false;
     bool playing = false;
     
     private void Start()
     {
+        rects = new RectTransform[next_obj.Length];
         rectTransform = select_obj.GetComponent<RectTransform>();
+        for (int i = 0; i < next_obj.Length; i++)
+        {
+            rects[i] = next_obj[i].GetComponent<RectTransform>();
+        }
+        
+    }
+
+    public void NextVector()
+    {
+        if (playing)
+        {
+            return;
+        }
+        playing = true;
+        if (onFilter)
+        {
+            onFilter = false;
+            nextVector3 = rects[0].position;            
+        }
+        else
+        {
+            onFilter = true;
+            nextVector3 = rects[1].position;
+        }
+        Debug.Log(nextVector3);
+        filter_Canvas.SetActive(!filter_Canvas.activeSelf);
+        nonfilter_Canvas.SetActive(!nonfilter_Canvas.activeSelf);
+        StartCoroutine(MoveSelect_Obj(nextVector3));
         
     }
     public void PlayerModSelect_Btn()
@@ -28,24 +63,25 @@ public class PlayerMod_Toggle : MonoBehaviour
         if (playerMod.Equals(1))
         {
             playerMod = 2;
-            nextVector = 140f;
+            nextVector3 = rects[1].position;
         }
         else
         {
             playerMod = 1;
-            nextVector = -140f;
+            nextVector3 = rects[0].position;
         }
         Debug.Log(playerMod);
 
         //현재 이미지가 이동히야 할 위치 값을 넘겨주기
-        StartCoroutine(MoveSelect_Obj(nextVector));
+        StartCoroutine(MoveSelect_Obj(nextVector3));
     }
     
-    private IEnumerator MoveSelect_Obj(float nextVector)
+    private IEnumerator MoveSelect_Obj(Vector3 nextVector)
     {
-        
-        Vector3 nextTransform = new Vector3(rectTransform.position.x + nextVector, rectTransform.position.y);
-        
+
+        //Vector3 nextTransform = new Vector3(rectTransform.position.x + nextVector, rectTransform.position.y);
+        Vector3 nextTransform = nextVector3;
+
         float currntTime = 0f;
         
         //설정한 시간 값으로 PlayMod 이미지 이동
