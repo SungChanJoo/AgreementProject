fa82a841a0154b07ee4b22fd82329fdb62f7d3bb
JaeBack-HONG <khks33@naver.com>
2024-03-09 17:07:37 +0900

    [Fix]Profile_UI BirthDayInput,Lv_Btn
    생년월일 입력 시 제한 설정, 플레이어 데이터 레벨 선택 시 버튼 이벤트 추가
    앨범 사진 로드 시 패딩설정

--- Assets/1.Scene/HJB/3.Script/Camera/LoadImage.cs
+++ Assets/1.Scene/HJB/3.Script/Camera/LoadImage.cs
@@ -16,47 +16,64 @@ public class LoadImage : MonoBehaviour
     [SerializeField] private Image select_img;
     private GameObject[] image_obj; 
     private Texture2D[] texture2Ds;
+    private bool loading = false;
 
     public void ImageFileLoad()
     {
+        if (!loading)
+        {
+            loading = true;
+            StartCoroutine(ImageFileLoad_Co());
+        }
+    }    
+    private IEnumerator ImageFileLoad_Co()
+    {
+        if (content_obj.transform.childCount>0)
+        {
+            GameObject[] children = new GameObject[content_obj.transform.childCount];
+            for (int i = 0; i < content_obj.transform.childCount; i++)
+            {
+                children[i] = content_obj.transform.GetChild(i).gameObject;
+            }
+            foreach (GameObject child in children)
+            {
+                Destroy(child);
+            }
+        }
         SettingManager.Instance.Re_AppSetPermission();
         string pathFolder = Application.persistentDataPath;
-        //string galaryPath = pathFolder.Substring(0, pathFolder.IndexOf("Android")) + "/DCIM/Camera/";
-        string galaryPath = $"{pathFolder}/";
-        string[] pngFiles = Directory.GetFiles(galaryPath,"*.jpg");
-        Texture2D[] textures = new Texture2D[pngFiles.Length];
-        Debug.Log(textures.Length);
-        image_obj = new GameObject[pngFiles.Length];
-        for (int i = 0; i < pngFiles.Length; i++)
+        string galleryPath = $"{pathFolder}/";
+        string[] imageFiles = Directory.GetFiles(galleryPath, "*.jpg");
+
+        texture2Ds = new Texture2D[imageFiles.Length];
+        image_obj = new GameObject[imageFiles.Length];
+
+        for (int i = 0; i < imageFiles.Length; i++)
         {
             image_obj[i] = Instantiate(image_prefeb);
             Button button = image_obj[i].GetComponent<Button>();
             int index = i;
             button.onClick.AddListener(() => SelectProfile_Image(index));
             image_obj[i].transform.SetParent(content_obj.transform);
-        }
 
-        for (int i = 0; i < pngFiles.Length; i++)
-        {
-            byte[] fileData = File.ReadAllBytes(pngFiles[i]);
+            byte[] fileData = File.ReadAllBytes(imageFiles[i]);
             Texture2D texture = new Texture2D(2, 2);
             texture.LoadImage(fileData);
-            Rect rect = new Rect(0, 0, texture.width, texture.height);                        
-            textures[i] = texture;
+            Rect rect = new Rect(0, 0, texture.width, texture.height);
+            texture2Ds[i] = texture;
             Image img = image_obj[i].GetComponent<Image>();
-            img.sprite = Sprite.Create(textures[i], rect, new Vector2(0.5f, 0.5f));
-        }        
-        texture2Ds = new Texture2D[pngFiles.Length];
-        texture2Ds = textures;
+            img.sprite = Sprite.Create(texture2Ds[i], rect, new Vector2(0.5f, 0.5f));
+        }
+        loading = false;
+        yield return new WaitForSeconds(0.1f);
+    }
 
-    }    
     public void SelectProfile_Image(int index)
     {
         Rect rect = new Rect(0, 0, texture2Ds[index].width, texture2Ds[index].height);
 
         profile_Img.sprite = Sprite.Create(texture2Ds[index], rect, new Vector2(0.5f, 0.5f));
-    }
-
+    }    
     public void Selectprofile_Change()
     {
         select_img.sprite = profile_Img.sprite;
