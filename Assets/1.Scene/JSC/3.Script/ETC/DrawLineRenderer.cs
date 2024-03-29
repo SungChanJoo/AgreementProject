fa36e7b28462d4caa6620af6d6fa3dd3a6c90449
SungChanJoo <scjoo123@naver.com>
2024-03-29 10:38:41 +0900

    [fix] 변수명 수정

--- Assets/1.Scene/JSC/3.Script/ETC/DrawLineRenderer.cs
+++ Assets/1.Scene/JSC/3.Script/ETC/DrawLineRenderer.cs
@@ -4,8 +4,8 @@ using UnityEngine;
 
 public class DrawLineRenderer : MonoBehaviour
 {
-    [SerializeField] private List<GameObject> StepPos;
-    [SerializeField] private float Depth = 6f;
+    [SerializeField] private List<GameObject> stepPos;
+    [SerializeField] private float depth = 6f;
     private LineRenderer lr;
 
     private void Awake()
@@ -20,11 +20,11 @@ public class DrawLineRenderer : MonoBehaviour
     {
         if (lr.enabled == false)
             lr.enabled = true;
-        lr.positionCount = StepPos.Count;
+        lr.positionCount = stepPos.Count;
         lr.loop = false;
-        for (int i = 0; i < StepPos.Count; i++)
+        for (int i = 0; i < stepPos.Count; i++)
         {
-            var position = StepPos[i].transform.position + new Vector3(0, 0, Depth);
+            var position = stepPos[i].transform.position + new Vector3(0, 0, depth);
             lr.SetPosition(i, position);
         }
     }
--- Assets/1.Scene/JSC/3.Script/Manager/RankingManager.cs
+++ Assets/1.Scene/JSC/3.Script/Manager/RankingManager.cs
@@ -7,64 +7,64 @@ using UnityEngine.UI;
 
 public class PlayerTimeRankingData
 {
-    public Image img;
-    public TMP_Text nameText;
-    public TMP_Text totalTimeText;
+    public Image Img;
+    public TMP_Text NameText;
+    public TMP_Text TotalTimeText;
     public PlayerTimeRankingData(Image img, TMP_Text nameText, TMP_Text totalTimeText)
     {
-        this.img = img;
-        this.nameText = nameText;
-        this.totalTimeText = totalTimeText;
+        this.Img = img;
+        this.NameText = nameText;
+        this.TotalTimeText = totalTimeText;
     }
 }
 public class PlayerScoreRankingData
 {
-    public Image img;
-    public TMP_Text nameText;
-    public TMP_Text totalScoreText;
+    public Image Img;
+    public TMP_Text NameText;
+    public TMP_Text TotalScoreText;
     public PlayerScoreRankingData(Image img, TMP_Text nameText, TMP_Text totalScoreText)
     {
-        this.img = img;
-        this.nameText = nameText;
-        this.totalScoreText = totalScoreText;
+        this.Img = img;
+        this.NameText = nameText;
+        this.TotalScoreText = totalScoreText;
     }
 }
 public class RankingManager : MonoBehaviour
 {
     //View UI
-    [SerializeField] private List<GameObject> TimeRankUI;
-    [SerializeField] private List<GameObject> ScoreRankUI;
+    [SerializeField] private List<GameObject> timeRankUI;
+    [SerializeField] private List<GameObject> scoreRankUI;
 
-    [SerializeField] private GameObject[] TimeRankPlayerPanel;
+    [SerializeField] private GameObject[] timeRankPlayerPanel;
     private PlayerTimeRankingData[] timeRankData;// 랭크 입력 필드
 
-    [SerializeField] private GameObject[] ScoreRankPlayerPanel;
+    [SerializeField] private GameObject[] scoreRankPlayerPanel;
     private PlayerScoreRankingData[] scoreRankData; // 랭크 입력 필드
-
+    [Header("PersnalData")]
     //개인 데이터 저장
-    [SerializeField] private TMP_Text Weekly;
-    [SerializeField] private TMP_Text Name;
-    [SerializeField] private GameObject[] PersnalRank;
-    [SerializeField] private Image MyImg;
+    [SerializeField] private TMP_Text weekly;
+    [SerializeField] private TMP_Text playerName;
+    [SerializeField] private GameObject[] persnalRank;
+    [SerializeField] private Image myImg;
     private void Awake()
     {
         timeRankData = new PlayerTimeRankingData[5];
-        for (int i = 0; i < TimeRankPlayerPanel.Length; i++)
+        for (int i = 0; i < timeRankPlayerPanel.Length; i++)
         {
-            var img = TimeRankPlayerPanel[i].transform.GetChild(1).GetComponent<Image>();
-            var name = TimeRankPlayerPanel[i].transform.Find("Name").GetChild(0).GetComponent<TMP_Text>();
-            var totalTime = TimeRankPlayerPanel[i].transform.Find("Total_Time").GetChild(0).GetComponent<TMP_Text>();
+            var img = timeRankPlayerPanel[i].transform.GetChild(1).GetComponent<Image>();
+            var name = timeRankPlayerPanel[i].transform.Find("Name").GetChild(0).GetComponent<TMP_Text>();
+            var totalTime = timeRankPlayerPanel[i].transform.Find("Total_Time").GetChild(0).GetComponent<TMP_Text>();
 
             var playerData = new PlayerTimeRankingData(img, name, totalTime);
             timeRankData[i] = playerData;
         }
 
         scoreRankData = new PlayerScoreRankingData[5];
-        for (int i = 0; i < ScoreRankPlayerPanel.Length; i++)
+        for (int i = 0; i < scoreRankPlayerPanel.Length; i++)
         {
-            var img = ScoreRankPlayerPanel[i].transform.GetChild(1).GetComponent<Image>();
-            var name = ScoreRankPlayerPanel[i].transform.Find("Name").GetChild(0).GetComponent<TMP_Text>();
-            var totalScore = ScoreRankPlayerPanel[i].transform.Find("Total_Score").GetChild(0).GetComponent<TMP_Text>();
+            var img = scoreRankPlayerPanel[i].transform.GetChild(1).GetComponent<Image>();
+            var name = scoreRankPlayerPanel[i].transform.Find("Name").GetChild(0).GetComponent<TMP_Text>();
+            var totalScore = scoreRankPlayerPanel[i].transform.Find("Total_Score").GetChild(0).GetComponent<TMP_Text>();
 
             var playerData = new PlayerScoreRankingData(img, name, totalScore);
             scoreRankData[i] = playerData;
@@ -83,30 +83,30 @@ public class RankingManager : MonoBehaviour
             var rankInfo = DataBase.Instance.PlayerCharacter[DataBase.Instance.CharacterIndex].RankingInfo;
             for (int i = 0; i < timeRankData.Length; i++)
             {
-                timeRankData[i].img.sprite = ProfileImage_Set(rankInfo.rankdata_time[^(i + 2)].userProfile);
-                timeRankData[i].nameText.text = rankInfo.rankdata_time[^(i + 2)].userName;
-                timeRankData[i].totalTimeText.text = $"{rankInfo.rankdata_time[^(i + 2)].totalTime}";
+                timeRankData[i].Img.sprite = ProfileImage_Set(rankInfo.rankdata_time[^(i + 2)].userProfile);
+                timeRankData[i].NameText.text = rankInfo.rankdata_time[^(i + 2)].userName;
+                timeRankData[i].TotalTimeText.text = $"{rankInfo.rankdata_time[^(i + 2)].totalTime}";
             }
 
             for (int i = 0; i < scoreRankData.Length; i++)
             {
-                scoreRankData[i].img.sprite = ProfileImage_Set(rankInfo.rankdata_time[^(i + 2)].userProfile);
-                scoreRankData[i].nameText.text =          rankInfo.rankdata_score[^(i + 2)].userName;
-                scoreRankData[i].totalScoreText.text = $"{rankInfo.rankdata_score[^(i + 2)].totalScore}";
+                scoreRankData[i].Img.sprite = ProfileImage_Set(rankInfo.rankdata_time[^(i + 2)].userProfile);
+                scoreRankData[i].NameText.text =          rankInfo.rankdata_score[^(i + 2)].userName;
+                scoreRankData[i].TotalScoreText.text = $"{rankInfo.rankdata_score[^(i + 2)].totalScore}";
             }
         }
         else
         {
             for (int i = 0; i < timeRankData.Length; i++)
             {
-                timeRankData[i].nameText.text = $"{i}";
-                timeRankData[i].totalTimeText.text = $"99시간99분{i}";
+                timeRankData[i].NameText.text = $"{i}";
+                timeRankData[i].TotalTimeText.text = $"99시간99분{i}";
             }
 
             for (int i = 0; i < scoreRankData.Length; i++)
             {
-                scoreRankData[i].nameText.text = $"{i}";
-                scoreRankData[i].totalScoreText.text = $"{i}9999";
+                scoreRankData[i].NameText.text = $"{i}";
+                scoreRankData[i].TotalScoreText.text = $"{i}9999";
             }
         }
 
@@ -117,11 +117,11 @@ public class RankingManager : MonoBehaviour
         {
             var rankInfo = DataBase.Instance.playerInfo.RankingInfo;
             var myData = rankInfo.rankdata_score[5];
-            Weekly.text = $"기간 : {rankInfo.periodDate}";
-            MyImg.sprite = ProfileImage_Set(DataBase.Instance.playerInfo.image);
-            Name.text = $"이름 : {DataBase.Instance.playerInfo.playerName}";
-            var timeRank = PersnalRank[0].transform.Find("TimeRank").GetChild(0).GetComponent<TMP_Text>();
-            var totalTime = PersnalRank[0].transform.Find("TotalTime").GetChild(0).GetComponent<TMP_Text>();
+            weekly.text = $"기간 : {rankInfo.periodDate}";
+            myImg.sprite = ProfileImage_Set(DataBase.Instance.playerInfo.image);
+            playerName.text = $"이름 : {DataBase.Instance.playerInfo.playerName}";
+            var timeRank = persnalRank[0].transform.Find("TimeRank").GetChild(0).GetComponent<TMP_Text>();
+            var totalTime = persnalRank[0].transform.Find("TotalTime").GetChild(0).GetComponent<TMP_Text>();
             timeRank.text = $"순위 : {myData.scorePlace}\n" +
                             $"최고순위 : {myData.highestTimePlace}";
 
@@ -129,19 +129,19 @@ public class RankingManager : MonoBehaviour
             var minute = myData.totalTime % 60;
             totalTime.text = $"누적시간\n {hour}시간 {minute}분";
 
-            var scoreRank = PersnalRank[1].transform.Find("ScoreRank").GetChild(0).GetComponent<TMP_Text>();
-            var totalScore = PersnalRank[1].transform.Find("TotalScore").GetChild(0).GetComponent<TMP_Text>();
+            var scoreRank = persnalRank[1].transform.Find("ScoreRank").GetChild(0).GetComponent<TMP_Text>();
+            var totalScore = persnalRank[1].transform.Find("TotalScore").GetChild(0).GetComponent<TMP_Text>();
             scoreRank.text = $"순위 : {myData.timePlace}\n" +
                             $"최고순위 : {myData.highestScorePlace}";
             totalScore.text = $"누적점수\n {myData.totalScore}";
         }
         else
         {
-            Weekly.text = $"기간 : {"1998.3.31~ 3.31"}";
-            Name.text = $"이름 : {"null"}";
+            weekly.text = $"기간 : {"1998.3.31~ 3.31"}";
+            playerName.text = $"이름 : {"null"}";
 
-            var timeRank = PersnalRank[0].transform.Find("TimeRank").GetChild(0).GetComponent<TMP_Text>();
-            var totalTime = PersnalRank[0].transform.Find("TotalTime").GetChild(0).GetComponent<TMP_Text>();
+            var timeRank = persnalRank[0].transform.Find("TimeRank").GetChild(0).GetComponent<TMP_Text>();
+            var totalTime = persnalRank[0].transform.Find("TotalTime").GetChild(0).GetComponent<TMP_Text>();
             var num = 10;
 
             timeRank.text = $"순위 : {num}\n" +
@@ -149,8 +149,8 @@ public class RankingManager : MonoBehaviour
             totalTime.text = $"누적시간\n {num}시간 {num}분";
 
 
-            var scoreRank = PersnalRank[1].transform.Find("ScoreRank").GetChild(0).GetComponent<TMP_Text>();
-            var totalScore = PersnalRank[1].transform.Find("TotalScore").GetChild(0).GetComponent<TMP_Text>();
+            var scoreRank = persnalRank[1].transform.Find("ScoreRank").GetChild(0).GetComponent<TMP_Text>();
+            var totalScore = persnalRank[1].transform.Find("TotalScore").GetChild(0).GetComponent<TMP_Text>();
             var num1 = 20;
 
             scoreRank.text = $"순위 : {num1}\n" +
@@ -162,13 +162,13 @@ public class RankingManager : MonoBehaviour
 
     public void ToggleRanking(bool enable)
     {
-        for (int i = 0; i < TimeRankUI.Count; i++)
+        for (int i = 0; i < timeRankUI.Count; i++)
         {
-            TimeRankUI[i].SetActive(enable);
+            timeRankUI[i].SetActive(enable);
         }
-        for (int i = 0; i < ScoreRankUI.Count; i++)
+        for (int i = 0; i < scoreRankUI.Count; i++)
         {
-            ScoreRankUI[i].SetActive(!enable);
+            scoreRankUI[i].SetActive(!enable);
         }
     }
     public Sprite ProfileImage_Set(byte[] img)
